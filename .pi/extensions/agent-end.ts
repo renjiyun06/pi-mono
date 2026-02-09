1c05b11222764707eabf9a3658e72c95ef08cd9a
renjiyun06 <renjiyun06@gmail.com>
2026-02-09 10:51:14 +0800

    disable agent_end notification

--- .pi/extensions/agent-end.ts
+++ .pi/extensions/agent-end.ts
@@ -2,6 +2,6 @@ import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
 
 export default function agentEndExtension(pi: ExtensionAPI): void {
 	pi.on("agent_end", async (_event, ctx) => {
-		ctx.ui.notify("agent_end received");
+		// ctx.ui.notify("agent_end received");
 	});
 }
