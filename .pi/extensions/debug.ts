0b2169b28ae2f5ee1bcd1c6e6d283c7aac0e7ef2
renjiyun <wozaijixu@msn.cn>
2026-03-13 21:59:34 +0800

    add debug extension for tool event logging

---
+++ .pi/extensions/debug.ts
@@ -0,0 +1,35 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+import { getLogger } from "@mariozechner/pi-logger";
+
+const log = getLogger("debug-extension");
+
+export default function (pi: ExtensionAPI) {
+	pi.on("tool_call", async (event) => {
+		log.info({ toolName: event.toolName, toolCallId: event.toolCallId, input: event.input }, "tool_call");
+	});
+
+	pi.on("tool_execution_start", async (event) => {
+		log.info({ toolName: event.toolName, toolCallId: event.toolCallId, args: event.args }, "tool_execution_start");
+	});
+
+	pi.on("tool_execution_update", async (event) => {
+		log.info(
+			{ toolName: event.toolName, toolCallId: event.toolCallId, args: event.args, partialResult: event.partialResult },
+			"tool_execution_update"
+		);
+	});
+
+	pi.on("tool_execution_end", async (event) => {
+		log.info(
+			{ toolName: event.toolName, toolCallId: event.toolCallId, result: event.result, isError: event.isError },
+			"tool_execution_end"
+		);
+	});
+
+	pi.on("tool_result", async (event) => {
+		log.info(
+			{ toolName: event.toolName, toolCallId: event.toolCallId, input: event.input, content: event.content, details: event.details, isError: event.isError },
+			"tool_result"
+		);
+	});
+}
