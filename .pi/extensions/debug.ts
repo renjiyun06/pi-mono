ef86d27d1e3608d77c396f8bfc20917e76967e4f
renjiyun <wozaijixu@msn.cn>
2026-03-14 13:44:02 +0800

    chore: update debug extension

--- .pi/extensions/debug.ts
+++ .pi/extensions/debug.ts
@@ -5,31 +5,31 @@ const log = getLogger("debug-extension");
 
 export default function (pi: ExtensionAPI) {
 	pi.on("tool_call", async (event) => {
-		log.info({ toolName: event.toolName, toolCallId: event.toolCallId, input: event.input }, "tool_call");
+		// log.info({ toolName: event.toolName, toolCallId: event.toolCallId, input: event.input }, "tool_call");
 	});
 
 	pi.on("tool_execution_start", async (event) => {
-		log.info({ toolName: event.toolName, toolCallId: event.toolCallId, args: event.args }, "tool_execution_start");
+		// log.info({ toolName: event.toolName, toolCallId: event.toolCallId, args: event.args }, "tool_execution_start");
 	});
 
 	pi.on("tool_execution_update", async (event) => {
-		log.info(
-			{ toolName: event.toolName, toolCallId: event.toolCallId, args: event.args, partialResult: event.partialResult },
-			"tool_execution_update"
-		);
+		// log.info(
+		// 	{ toolName: event.toolName, toolCallId: event.toolCallId, args: event.args, partialResult: event.partialResult },
+		// 	"tool_execution_update"
+		// );
 	});
 
 	pi.on("tool_execution_end", async (event) => {
-		log.info(
-			{ toolName: event.toolName, toolCallId: event.toolCallId, result: event.result, isError: event.isError },
-			"tool_execution_end"
-		);
+		// log.info(
+		// 	{ toolName: event.toolName, toolCallId: event.toolCallId, result: event.result, isError: event.isError },
+		// 	"tool_execution_end"
+		// );
 	});
 
 	pi.on("tool_result", async (event) => {
-		log.info(
-			{ toolName: event.toolName, toolCallId: event.toolCallId, input: event.input, content: event.content, details: event.details, isError: event.isError },
-			"tool_result"
-		);
+		// log.info(
+		// 	{ toolName: event.toolName, toolCallId: event.toolCallId, input: event.input, content: event.content, details: event.details, isError: event.isError },
+		// 	"tool_result"
+		// );
 	});
 }
